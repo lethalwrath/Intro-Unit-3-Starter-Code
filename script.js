4665ee69c134007f2dee5e2eb6f91cf32ed9a7a3
anguianosebasti <24334886-anguianosebasti@users.noreply.replit.com>
2023-08-02 17:17:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let body = document.querySelector("body")
+body.style.backgroundColor = "Black";
+body.style.fontColor="red"
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
